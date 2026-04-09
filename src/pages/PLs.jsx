7f024f43c294d31fd6ea52502355e72575ea78cf
tristankernans <tristan@kernans.ie>
2026-04-09 14:11:57 +0100

    back

--- src/pages/PLs.jsx
+++ src/pages/PLs.jsx
@@ -1,5 +1,4 @@
 import { Link, useNavigate } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
 
 const tiles = [
   { label: "FRUIT & VEG P+L", to: "/fruitveg", icon: "🍎" },
